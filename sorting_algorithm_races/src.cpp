90a8ba270db19921e4c32abf7846c6ab5298073a
the94zyskowski <maciej.zyskowski.professional@gmail.com>
2024-09-18 10:01:23 +0200

    Tidying up. Some encapsulation.

--- sorting_algorithm_races/src.cpp
+++ sorting_algorithm_races/src.cpp
@@ -1,18 +1,5 @@
-﻿#include <vector>
-#include <set>
-#include <cstdlib>
-#include <iostream>
-#include <algorithm>
-#include <chrono>
-#include <thread>
-#include <mutex>
-#include <string>
-#include <stdexcept>
-#include <unordered_map>
-#include "bubble_sort.h"
-
-std::mutex mtx;  // Mutex do synchronizacji dostępu do listy wyników
-std::vector<std::string> results;  // Lista wyników, gdzie zapisywane są identyfikatory algorytmów
+﻿#include "bubble_sort.h"
+#include "src.h"
 
 void create_pool(std::vector<int>& v) {
     std::set<int> unique_numbers;
@@ -33,18 +20,9 @@ void show_first_30_numbers(std::vector<int>& v) {
     std::cout << std::endl;
 }
 
-class algorithm_ {
-private:
-    std::string name;
-    std::string info;
-    double sorting_time;
-    int sorted_elements;
-    int id;
 
-    static std::unordered_map<std::string, int> algorithm_map;
-public:
-    algorithm_(): name("none"), info("empty"), sorting_time(-1.0), sorted_elements(-1), id(-1) {}
-    algorithm_(std::string n, std::string i) : name(n), info(i), sorting_time(-1), sorted_elements(-1), id(-1) {
+algorithm_::algorithm_(): name("none"), info("empty"), sorting_time(-1.0), sorted_elements(-1), id(-1) {}
+algorithm_::algorithm_(std::string n, std::string i) : name(n), info(i), sorting_time(-1), sorted_elements(-1), id(-1) {
         if (algorithm_map.find(name) != algorithm_map.end()) {
             id = algorithm_map[name];
         }
@@ -52,9 +30,9 @@ public:
             throw std::invalid_argument("Algorithm not found. :(");
         }
     }
-    ~algorithm_() {}
+algorithm_::~algorithm_() {}
 
-    void go(std::vector<int> v) {
+void algorithm_::go(std::vector<int> v) {
         auto start = std::chrono::high_resolution_clock::now();
         if (id == 1) {
             std::sort(v.begin(), v.end());
@@ -79,42 +57,26 @@ public:
         sorted_elements = v.size();
     }
 
-    void set_name(const std::string n) { name = n; }
-    std::string const get_name() { return name; }
+void algorithm_::set_name(const std::string n) { name = n; }
+std::string const algorithm_::get_name() { return name; }
 
-    void set_id(const int n_id) { id = n_id; }
-    int const get_id() { return id; }
+void algorithm_::set_id(const int n_id) { id = n_id; }
+int const algorithm_::get_id() { return id; }
 
-    void set_sorting_time(const double st) { sorting_time = st; }
-    double const get_sorting_time() { return sorting_time; }
+void algorithm_::set_sorting_time(const double st) { sorting_time = st; }
+double const algorithm_::get_sorting_time() { return sorting_time; }
 
-    void set_sorted_elements(const int se) { sorted_elements = se; }
-    int const get_sorted_elements() { return sorted_elements; }
+void algorithm_::set_sorted_elements(const int se) { sorted_elements = se; }
+int const algorithm_::get_sorted_elements() { return sorted_elements; }
 
-    void set_info(const std::string i) { info = i; }
-    std::string const get_info() { return info; }
+void algorithm_::set_info(const std::string i) { info = i; }
+std::string const algorithm_::get_info() { return info; }
 
-    void print_all() { //This is nice mmhm
-        std::cout << "Algorithm name: " << (!name.empty() ? name : "No name") << std::endl;
-        std::cout << "Info: " << (!info.empty() ? info : "No info") << std::endl;
-        std::cout << "Sorted <" << (sorted_elements > 0 ? std::to_string(sorted_elements) : "No sorted elements") << "> in " << (sorting_time > 0 ? std::to_string(sorting_time) : "No sorting time") << " seconds." << std::endl;
-    }
-};
-
-std::unordered_map<std::string, int> algorithm_::algorithm_map = {
-    {"quick sort", 1}, //I am aware there are more combinations, but this is not the focus of this project and should be sufficient for now.
-    {"Quick Sort", 1},
-    {"quicksort", 1},
-    {"stable sort", 2},
-    {"Stable Sort", 2},
-    {"stablesort", 2},
-    {"heap sort", 3},
-    {"Heap Sort", 3},
-    {"heapsort", 3},
-    {"bubble sort", 4},
-    {"Bubble Sort", 4},
-    {"bubblesort", 4}
-};
+void algorithm_::print_all() { //This is nice mmhm
+    std::cout << "Algorithm name: " << (!name.empty() ? name : "No name") << std::endl;
+    std::cout << "Info: " << (!info.empty() ? info : "No info") << std::endl;
+    std::cout << "Sorted <" << (sorted_elements > 0 ? std::to_string(sorted_elements) : "No sorted elements") << "> in " << (sorting_time > 0 ? std::to_string(sorting_time) : "No sorting time") << " seconds." << std::endl;
+}
 
 int main() {
     std::vector<int> vec;
---
+++ sorting_algorithm_races/src.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+#include <vector>
+#include <set>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <stdexcept>
+#include <unordered_map>
+#include "bubble_sort.h"
+
+std::mutex mtx;  // Mutex do synchronizacji dostępu do listy wyników
+std::vector<std::string> results;  // Lista wyników, gdzie zapisywane są identyfikatory algorytmów
+
+void create_pool(std::vector<int>& v);
+
+void show_first_30_numbers(std::vector<int>& v);
+
+class algorithm_ {
+private:
+    std::string name;
+    std::string info;
+    double sorting_time;
+    int sorted_elements;
+    int id;
+
+    static std::unordered_map<std::string, int> algorithm_map;
+public:
+    algorithm_();
+    algorithm_(std::string n, std::string i);
+    ~algorithm_();
+
+    void go(std::vector<int> v);
+
+    void set_name(const std::string n);
+    std::string const get_name();
+
+    void set_id(const int n_id);
+    int const get_id();
+
+    void set_sorting_time(const double st);
+    double const get_sorting_time();
+
+    void set_sorted_elements(const int se);
+    int const get_sorted_elements();
+
+    void set_info(const std::string i);
+    std::string const get_info();
+
+    void print_all();
+};
+
+std::unordered_map<std::string, int> algorithm_::algorithm_map = {
+    {"quick sort", 1}, //I am aware there are more combinations, but this is not the focus of this project and should be sufficient for now.
+    {"Quick Sort", 1},
+    {"quicksort", 1},
+    {"stable sort", 2},
+    {"Stable Sort", 2},
+    {"stablesort", 2},
+    {"heap sort", 3},
+    {"Heap Sort", 3},
+    {"heapsort", 3},
+    {"bubble sort", 4},
+    {"Bubble Sort", 4},
+    {"bubblesort", 4}
+};
